ce08bf105e72334ebaf6866d8446f8bef1bf31ec
Carl Smith <carlfsmithiii@gmail.com>
2018-11-21 06:33:13 -1000

    init src/actions, src/constants, and src/reducers directories; create freezer action and reducer

---
+++ src/reducers/freezer.spec.js
@@ -0,0 +1,10 @@
+import reducer from './freezer';
+
+import { updateTemperature } from '../actions/freezer';
+
+describe('Freezer reducer', function() {
+    it('should store the temperature in the state', function() {
+        const newState = reducer(undefined, updateTemperature(-5));
+        expect(newState.temperature).toEqual(-5);
+    });
+});
