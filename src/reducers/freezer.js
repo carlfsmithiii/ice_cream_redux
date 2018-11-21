83180de6d2840455ccdd2162418ae11591cbc4e1
Carl Smith <carlfsmithiii@gmail.com>
2018-11-21 07:17:37 -1000

    added reducers and tests for freezer inventory

--- src/reducers/freezer.js
+++ src/reducers/freezer.js
@@ -1,7 +1,8 @@
-import { UPDATE_TEMPERATURE } from '../constants/freezer';
+import { ADD_PRODUCT_TO_FREEZER, UPDATE_TEMPERATURE } from '../constants/freezer';
 
 const DEFAULT_STATE = {
     temperature: null,
+    flavors: {},
 };
 
 export default function freezerReducer(state = DEFAULT_STATE, action) {
@@ -11,6 +12,15 @@ export default function freezerReducer(state = DEFAULT_STATE, action) {
                 ...state,
                 temperature: action.payload,
             };
+        case ADD_PRODUCT_TO_FREEZER:
+            const amount = (state.flavors[action.payload.name] || 0) + action.payload.amount;
+            return {
+                ...state, 
+                flavors: {
+                    ...state.flavors,
+                    [action.payload.name]: Math.min(amount, 60)
+                },
+            }
     }
     return state;
 }
