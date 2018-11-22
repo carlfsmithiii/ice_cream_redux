ad290f21eb07a74a8bfd3a1279e31b8f5dfae5fe
Carl Smith <carlfsmithiii@gmail.com>
2018-11-21 14:06:48 -1000

    created shell of employees duck

---
+++ src/ducks/employees.js
@@ -0,0 +1,18 @@
+export const types = {
+
+};
+
+const DEFAULT_STATE = {
+
+};
+
+export function reducer(state = DEFAULT_STATE, action) {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
+
+export const actions = {
+
+};
