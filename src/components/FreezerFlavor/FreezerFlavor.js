657698551669d90d4a2e8703869ae5929f5df1ff
Carl Smith <carlfsmithiii@gmail.com>
2018-11-23 10:20:00 -1000

    added ability to remove scoops from store and tests

--- src/components/FreezerFlavor/FreezerFlavor.js
+++ src/components/FreezerFlavor/FreezerFlavor.js
@@ -4,14 +4,14 @@ import Button from '../Button/Button';
 
 import './FreezerFlavor.css';
 
-const FreezerFlavor = ({ flavorName, scoops, onClickRestock }) => {
+const FreezerFlavor = ({ flavorName, scoops, onClickRestock, onClickFlavor }) => {
   return (
     <div className="freezer-flavor">
-      <div className="freezer-flavor__inner">
+      <div className="freezer-flavor__inner" onClick={onClickFlavor}>
         <div className="freezer-flavor__scoops-counter">{scoops}</div>
         <div className="freezer-flavor__name">{flavorName}</div>
       </div>
-      <Button onClick={onClickRestock} className="freezer-flavor__restock-button" label="Restock"/>
+      <Button onClick={onClickRestock} className="freezer-flavor__restock-button" label="Restock" />
     </div>
   );
 };
